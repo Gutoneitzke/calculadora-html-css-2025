c118ae5899db1d1ad33259db8bdc1067bdca778c
Gutoneitzke <gutoneitzke12345@gmail.com>
2025-01-25 12:57:59 -0300

    feat: new buttons

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+    document.getElementById('display').value += value
 }
 
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    document.getElementById('display').value = ''
 }
 
 function calculate() {
-    const display = document.getElementById('display');
+    const display = document.getElementById('display')
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value)
     } catch (error) {
-        display.value = 'Erro';
+        display.value = 'Erro'
     }
+}
+
+function back () {
+    const display = document.getElementById('display')
+    display.value = display.value.slice(0, -1)
 }
